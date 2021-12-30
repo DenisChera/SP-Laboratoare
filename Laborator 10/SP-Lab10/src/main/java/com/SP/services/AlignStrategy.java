d4a84f933ccf697255807939c2fd80ab4c175a6e
DenisChera <73703962+DenisChera@users.noreply.github.com>
2021-12-30 19:32:20 +0200

    Delete Laborator 10 directory

--- Laborator 10/SP-Lab10/src/main/java/com/SP/services/AlignStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.sabloane.services;
-
-import com.sabloane.models.Context;
-import com.sabloane.models.Paragraph;
-
-public interface AlignStrategy {
-    void render(Paragraph paragraph, Context context);
-}
